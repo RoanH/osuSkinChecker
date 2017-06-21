b687ea17dd312d7b4e12f4cfa145ade9a4b9d7a8
RoanH <RoanH@192.168.178.32>
2017-06-21 21:08:11 +0200

    Initial commit

---
+++ osuSkinChecker/src/me/roan/osuskinchecker/SkinChecker.java
@@ -0,0 +1,19 @@
+package me.roan.osuskinchecker;
+
+public class SkinChecker {
+
+	//https://puu.sh/woI9Q/03bc8befea.txt
+	//https://docs.google.com/spreadsheets/d/1bhnV-CQRMy3Z0npQd9XSoTdkYxz0ew5e648S00qkJZ8/edit#gid=2074725196
+	
+	//options:
+	//N=variable amount
+	//M=variable amount not preceded by a -
+	//S=single image only no higher/lower res
+	//C=custom path
+	//L=legacy
+	//-= n/a
+
+	public static void main(String[] args){
+
+	}
+}
