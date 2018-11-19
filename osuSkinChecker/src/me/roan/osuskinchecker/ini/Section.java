69074c7410b3e1c3828bcf90a3ac800ea99e4b1e
Roan <roan.hofland@hotmail.com>
2018-11-19 18:18:21 +0100

    Add a javadoc to the Section class

--- osuSkinChecker/src/me/roan/osuskinchecker/ini/Section.java
+++ osuSkinChecker/src/me/roan/osuskinchecker/ini/Section.java
@@ -5,16 +5,40 @@ import java.util.List;
 
 import me.roan.osuskinchecker.ini.SkinIni.ManiaIni;
 
+/**
+ * Represents a section in the skin.ini
+ * @author Roan
+ */
 public class Section{
-
+	/**
+	 * The name of this skin.ini section
+	 */
 	protected final String name;
+	/**
+	 * The mania configuration for this
+	 * section if this is a mania section
+	 */
 	protected ManiaIni mania;
+	/**
+	 * The settings under this section
+	 */
 	protected final List<Setting<?>> data = new ArrayList<Setting<?>>();
 	
+	/**
+	 * Constructs a new section with
+	 * the given name
+	 * @param name The name for this section
+	 */
 	protected Section(String name){
 		this.name = name;
 	}
 	
+	/**
+	 * Checks if this section is a
+	 * mania section
+	 * @return Whether or not this section
+	 *         is a mania section
+	 */
 	protected boolean isMania(){
 		return mania != null || (name != null && name.trim().equals("[Mania]"));
 	}
